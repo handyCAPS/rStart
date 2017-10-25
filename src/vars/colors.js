b50dfffbad28cd1edcd3e3c2a2e77f79035a05ff
Tim Doppenberg <info@timdoppenberg.nl>
2017-10-25 13:55:08 +0200

    Adds Blues colors

--- src/vars/colors.js
+++ src/vars/colors.js
@@ -6,3 +6,11 @@ export const Colors = {
 	darkAccent: '#869CA9',
 	darkShade: '#357DA9'
 };
+
+export const Blues = {
+	light: '#00A6FB',
+	base: '#357DA9',
+	medium: '#006494',
+	dark: '#003554',
+	black: '#051923'
+};
